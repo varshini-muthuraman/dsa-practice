7e79a59f4535da9dbdc7b717dbfe6ed31c091dcf
Varshini Muthuraman <varshini.muthuraman06@gmail.com>
2026-05-26 23:35:23 +0530

    solution of Encode and Decode strings

---
+++ encode_decode_strings.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s = "".join(f"{len(s)}#{s}"for s in strs)
+        return s
+    def decode(self, s: str) -> List[str]:
+        res,i = [],0
+        while i<len(s):
+            j = s.index("#",i)
+            length = int(s[i:j])
+            res.append(s[j+1:j+1+length])
+            i = j+1+length
+
+        return res
+            
